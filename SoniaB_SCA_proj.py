0ec04bf66e7840482e8e1b8086fb2d59e0a4494c
SoniaBendre <65863659+SoniaBendre@users.noreply.github.com>
2021-05-23 14:27:22 +0100

    Uploaded project file

---
+++ SoniaB_SCA_proj.py
@@ -0,0 +1,16 @@
+import re
+
+# Asks user for statement that contains profanity which needs to be removed
+input_text = input("Type the statement to remove profanity from.")
+
+# States the profanity to check for
+profanity_record = "fuck|shit|bitch"
+
+# Removes all the profanity in "input_text"
+profanity_removed = re.sub(profanity_record, "", input_text)
+
+# Removes unnecessary spaces from "profanity_found"
+refined_result = re.sub(" ", "", profanity_removed)
+
+# Prints the refined_result
+print(refined_result)
